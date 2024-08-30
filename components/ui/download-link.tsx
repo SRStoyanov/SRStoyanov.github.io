ab6d53da77819a2a1325c9bbe11f960c1194466b
Sava Stoyanov <stoyanov.sava@gmail.com>
2024-08-30 12:21:59 +0300

    resume update

--- components/ui/download-link.tsx
+++ components/ui/download-link.tsx
@@ -2,7 +2,7 @@ import { Download } from "lucide-react";
 
 export function DownloadLink() {
   return (
-    <a href="/Stoyanov-Resume-24.pdf" download>
+    <a href="/Stoyanov-Resume.pdf" download>
       <Download className="hidden sm:inline-block mr-2 h-4 w-4" /> Resume
     </a>
   );
